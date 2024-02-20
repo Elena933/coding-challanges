a2f24d831451dea5dd5e64589122905c2107917e
Elena933 <76497529+Elena933@users.noreply.github.com>
2024-02-20 04:44:32 +0000

    directory

--- quadratic time/findTheSumElemInArr.js
+++
@@ -1,9 +0,0 @@
-function findTheSumElemInArr(arr){
-   let sum = 0;
-   for(let i = 0; i< arr.length; i++){
-      sum += arr[i]
-   }
-   return sum
-}
-const arr = [2, 2]
-console.log(findTheSumElemInArr(arr))
--- quadratic time/remove3rdElem.js
+++
@@ -1,8 +0,0 @@
-const arr = [5, 8, 4, 7]
-function remove3rdElem(arr){
-  arr.splice(2, 1)
-return arr
-  
-}
-console.log(remove3rdElem(arr))
-// splice has to shift elements
